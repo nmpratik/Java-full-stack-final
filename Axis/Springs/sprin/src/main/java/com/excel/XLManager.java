a2a9ed4860adc98fae095239af9f214597a570e8
nmpratik <nmprathik@gmail.com>
2023-04-03 13:17:32 +0530

    spring: bean

---
+++ Axis/Springs/sprin/src/main/java/com/excel/XLManager.java
@@ -0,0 +1,16 @@
+package com.excel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class XLManager {
+
+    private XLReader reader;
+    private XLFormatter formatter;
+
+    public void configXL() {
+        reader.setReadLimit(10);
+        formatter.setFont("abc");
+    }
+}
+
---
+++ Axis/Springs/sprin/src/main/java/com/excel/XlConfig.java
@@ -0,0 +1,9 @@
+package com.excel;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "com.ani.spring")
+public class XlConfig {
+    
+}
+
