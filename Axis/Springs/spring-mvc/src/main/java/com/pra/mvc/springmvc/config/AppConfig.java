a8abb8d669866b3cc2b096c643cf8ce8c5c7dad2
nmpratik <nmprathik@gmail.com>
2023-04-04 12:15:48 +0530

    spring mvc

---
+++ Axis/Springs/spring-mvc/src/main/java/com/pra/mvc/springmvc/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.pra.mvc.springmvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        registry.viewResolver(resolver);
+    }
+}
---
+++ Axis/Springs/spring-mvc/src/main/java/com/pra/mvc/springmvc/controller/HiController.java
@@ -0,0 +1,16 @@
+package com.pra.mvc.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/hi")
+@Controller
+public class HiController {
+    
+    @GetMapping("/sayHi")
+    public String hi() {
+        return "hi";
+    }
+}
+
