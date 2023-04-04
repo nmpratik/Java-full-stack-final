4279b158e85ad4e5592eefc42ea5165f0e77af11
nmpratik <nmprathik@gmail.com>
2023-04-04 15:47:34 +0530

    validate update

--- Axis/Springs/spring-mvc/src/main/java/com/pra/mvc/springmvc/controller/UserController.java
+++ Axis/Springs/spring-mvc/src/main/java/com/pra/mvc/springmvc/controller/UserController.java
@@ -3,8 +3,10 @@ package com.pra.mvc.springmvc.controller;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.pra.mvc.springmvc.entity.User;
+import com.pra.mvc.springmvc.validation.UserValidator;
 
 @RequestMapping("/user")
 @Controller
 public class UserController {
     
+    @Autowired
+    private UserValidator validator;
+
     private final List<User> users = Arrays.asList(
         new User(11, "abc", "982275245", "aa@bb.com"),
         new User(12, "pqr", "54543543", "cc@ww.com"),
@@ -40,7 +46,14 @@ public class UserController {
     }
 
     @PostMapping("/create") // http://localhost:8080/user/create
-    public ModelAndView createNewUser(@ModelAttribute("user") User user) {
+    public ModelAndView createNewUser(@ModelAttribute("user") User user, BindingResult result) {
+
+        validator.validate(user, result);
+
+        if(result.hasErrors()) {
+            return new ModelAndView("fail");
+        }
+
 
         ModelAndView mv = new ModelAndView("success");
         mv.addObject("usNm", user.getName());
