cf3a00287e8d728d67a0cf7877f67d1d9788b08e
nmpratik <nmprathik@gmail.com>
2023-04-08 16:07:11 +0530

    safe commit

---
+++ Axis/Springs/reactive-mongo-demo/src/main/java/com/pra/reactive/mongo/ReactiveMongoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pra.reactive.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMongoDemoApplication.class, args);
+	}
+
+}
--- Axis/Springs/spring-flux/src/main/java/com/pra/flux/SpringFluxApplication.java
+++ Axis/Springs/spring-flux/src/main/java/com/pra/flux/SpringFluxApplication.java
@@ -2,12 +2,19 @@ package com.pra.flux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.pra.flux.demo.ReactiveComponents;
 
 @SpringBootApplication
 public class SpringFluxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringFluxApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringFluxApplication.class, args);
+
+		ReactiveComponents cp =  ctx.getBean(ReactiveComponents.class);
+		cp.monoFluxDemo1();
 	}
+
 
 }
