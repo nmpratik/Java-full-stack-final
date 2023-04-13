e5d4c25c31c33fc39bc33f33b0b22f9af7e3bac1
nmpratik <nmprathik@gmail.com>
2023-04-13 12:11:32 +0530

    jaxb

---
+++ Axis/Springs/format-transform-demo/src/main/java/com/pra/transform/FormatTransformDemoApplication.java
@@ -0,0 +1,57 @@
+package com.pra.transform;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import javax.xml.bind.JAXBException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.pra.transform.domain.Author;
+import com.pra.transform.domain.Book;
+import com.pra.transform.domain.Publisher;
+import com.pra.transform.xml.Converter;
+
+@SpringBootApplication
+public class FormatTransformDemoApplication {
+
+	public static void main(String[] args) throws JAXBException, IOException {
+		ApplicationContext ctx = SpringApplication.run(FormatTransformDemoApplication.class, args);
+
+
+		Converter converter = ctx.getBean(Converter.class);
+
+
+		Author author = new Author();
+		author.setFirstName("abc");
+		author.setLastName("pqr");
+
+		Publisher publisher = new Publisher();
+		publisher.setLocation("london");
+		publisher.setName("pub");
+
+		Book book = new Book();
+		book.setGenre("abc");
+		book.setIsbn("6543gfh167245");
+		book.setTitle("tuv");
+		book.setAuthor(author);
+		book.setPublisher(publisher);
+		book.setPublishedDate(LocalDate.now());
+
+		try {
+			converter.toXml(book);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+
+
+		Resource res = new ClassPathResource("book.xml");
+		Book bk = converter.toDomain(res.getFile());
+		System.out.println(bk);
+	}
+
+}
